690ad547a357ad507e028b61a46d1e24f1d6d167
n_pang <n_pang@fancs.com>
2015-09-01 16:13:44 +0900

    output japanese hiragana

---
+++ hiragana/kana.php
@@ -0,0 +1,20 @@
+<?php
+//ひらがな
+$_kana[] = range(hexdec(e38181),hexdec(e381bf));
+$_kana[] = range(hexdec(e38280),hexdec(e38293));
+//カタカナ
+$_kana[] = range(hexdec(e382a1),hexdec(e382bf));
+$_kana[] = range(hexdec(e38380),hexdec(e383b6)); //カタカナ
+$kana_list = array();
+foreach($_kana as $no_use => $value) {
+	foreach($value as $no_use => $value)
+	$kana_list[dechex($value)] = pack("H*", dechex($value));
+}
+foreach($kana_list as $_k => $_v) {
+	print "$_k:$_v\n";
+}
+//アルファベット
+$alphabet_list = range(a,z);
+foreach($alphabet_list as $_k => $_v) {
+	print "$_k:$_v\n";
+}
